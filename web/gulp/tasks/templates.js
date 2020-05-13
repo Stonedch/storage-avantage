6a04f5b480a8ea5eded720e322da46ad3f0417e8
Maxim Mukhin <stch.max.muhin@gmail.com>
2020-05-13 18:45:31 +0300

    fix(front-end, gulp): update task "templates"

--- web/gulp/tasks/templates.js
+++ web/gulp/tasks/templates.js
@@ -6,7 +6,7 @@ const config = require("../config");
 gulp.task("templates", function(done) {
     gulp.src(config.templates.path + "**/*.pug")
         .pipe(pug({pretty: config.templates.beautifyHtml}))
-        .pipe(gulp.dest(config.paths.build));
+        .pipe(gulp.dest(config.paths.build + "templates"));
 
     done();
 });
