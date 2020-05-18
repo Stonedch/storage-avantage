16d38932863cc622c664f61303beec2a6ec77878
Maxim Mukhin <stch.max.muhin@gmail.com>
2020-05-18 18:14:35 +0300

    chore(front-end): update task "collect:collect"

--- storage/web/gulpfile.js
+++ storage/web/gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const rename = require("gulp-rename");
 
 const build = require("./gulp/tasks/build");
 const serve = require("./gulp/tasks/serve");
@@ -6,10 +7,13 @@ const config = require("./gulp/config");
 
 gulp.task("collect:collect", function(done) {
     gulp.src(config.paths.build + "**/*.html")
+        .pipe(rename({suffix: ".new"}))
         .pipe(gulp.dest("../templates/storage/"));
     gulp.src(config.paths.build + "**/*.css")
+        .pipe(rename({suffix: ".new"}))
         .pipe(gulp.dest("../static/storage/"));
     gulp.src(config.paths.build + "**/*.js")
+        .pipe(rename({suffix: ".new"}))
         .pipe(gulp.dest("../static/storage/"));
     gulp.src(config.paths.build + "images/**/*")
         .pipe(gulp.dest("../static/storage/images/"));
