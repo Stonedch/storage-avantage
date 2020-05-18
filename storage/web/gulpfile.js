0556610d9f8eb9db6202bd1088ec1bae5e1f0f32
Maxim Mukhin <stch.max.muhin@gmail.com>
2020-05-18 17:49:50 +0300

    chore(front-end): add new task "collect:collect"

--- storage/web/gulpfile.js
+++ storage/web/gulpfile.js
@@ -2,5 +2,19 @@ const gulp = require("gulp");
 
 const build = require("./gulp/tasks/build");
 const serve = require("./gulp/tasks/serve");
+const config = require("./gulp/config");
 
+gulp.task("collect:collect", function(done) {
+    gulp.src(config.paths.build + "**/*.html")
+        .pipe(gulp.dest("../templates/storage/"));
+    gulp.src(config.paths.build + "**/*.css")
+        .pipe(gulp.dest("../static/storage/"));
+    gulp.src(config.paths.build + "**/*.js")
+        .pipe(gulp.dest("../static/storage/"));
+    gulp.src(config.paths.build + "images/**/*")
+        .pipe(gulp.dest("../static/storage/images/"));
+
+    done();
+
+});
 gulp.task("default", gulp.series("build:build"))
